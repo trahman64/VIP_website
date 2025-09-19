8221cf7ba1671f0753b204288ebd218ec6604268
trahman64 <156981381+trahman64@users.noreply.github.com>
2025-09-19 04:34:15 +0000

    Deploying to gh-pages from @ trahman64/VIP_website@d024b2d79f903e88778f777910634539e2316f97 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/VIP_website/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/VIP_website/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
